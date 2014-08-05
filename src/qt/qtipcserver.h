5e2089e9012fc6c7abd132a7b6a7cc8ffea75939
CK <coin@carsencustoms.com>
2014-08-05 17:41:26 -0600

    Launch! 8/05/2014

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define CKcoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "CKcoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
